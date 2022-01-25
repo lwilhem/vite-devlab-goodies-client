d1d5b97d48eebf347fb69c87989fcaf8bd8b8930
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-01-25 15:08:20 +0100

    :construction: shop article list

---
+++ src/components/ShopItemList.vue
@@ -0,0 +1,44 @@
+<script>
+import { HashtagIcon, AnnotationIcon, BellIcon } from '@heroicons/vue/solid'
+
+export default {
+    components: {
+        HashtagIcon,
+        AnnotationIcon,
+        BellIcon
+    }
+}
+</script>
+
+<template>
+    <section class="w-full h-screen flex flex-col items-center justify-center"> <!-- SHOP WRAPPER -->
+        <div class="w-4/5 h-96 bg-slate-500 flex flex-col justify-evenly items-start p-8"> <!-- ASSOS PRESENTATIONS WRAPPER -->
+            <h2 class="w-full text-center text-6xl uppercase tracking-widest">Nom de L'assos</h2>
+            <div class="w-2/4"> <!-- PRESENTATION TEXT WRAPPER - ALL THE CONTENT IS GOING TO BE DINAMICALLY GENERATED THROUGH API CALLS-->
+                <p class="text-xm"> 
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                    invidunt ut labore et dolore magna
+                    <br><br>
+                    liquyam erat, sed diam voluptua. At vero eos et accusam et
+                    justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                    est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
+                </p>
+            </div>
+        </div>
+        <div class="w-full flex items-center justify-center h-10 my-4">
+            <a href=""> <!-- ICON & SOCIAL MEDIA LINK WRAPPER-->
+                <HashtagIcon class="w-8 h-8 mx-12"></HashtagIcon>
+            </a>
+            <a href="">
+                <AnnotationIcon class="w-8 h-8 mx-12"></AnnotationIcon>
+            </a>
+            <a href="">
+                <BellIcon class="w-8 h-8 mx-12"></BellIcon>
+            </a>
+        </div>
+    </section>
+
+    <section>
+
+    </section>
+</template>
