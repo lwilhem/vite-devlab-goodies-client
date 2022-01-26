bb75d2c0646a052a383c6fd588bb7607a56d5f95
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-01-26 14:42:02 +0100

    :lipstick: WIP - Navbar

--- src/components/utils/toolbox.js
+++ src/components/utils/toolbox.js
@@ -1,3 +1,10 @@
 export async function httpFetchRequest(query, variables = {}){
     console.log('function imported')
 }
+
+export class MenuDislay{
+    constructor(input, target){
+        this.input = input,
+        this.target = target
+    }
+}
