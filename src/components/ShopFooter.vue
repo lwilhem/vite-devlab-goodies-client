8e8b697d65cceccbeae862a42329b07596ceb1f6
romainmltr <romain_malaterre@hotmail.fr>
2022-02-22 16:57:47 +0100

    📦️add component Romain

---
+++ src/components/ShopFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  
+  <div class="footer flex justify-evenly bg-[url('src/assets/footer.svg')] bg-no-repeat bg-cover ">
+    <p class="">devlab</p>
+    <p>Mentions Legales</p>
+    <p>Nous Contacter</p>
+   
+  </div>
+
+
+</template>
