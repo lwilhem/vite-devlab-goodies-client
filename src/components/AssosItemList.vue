a23fc660da0421506f8f519ce7d55fb2b10e4b67
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-02-22 18:46:49 +0100

    :lipstick: added item pagination in shops

--- src/components/AssosItemList.vue
+++ src/components/AssosItemList.vue
@@ -24,5 +24,18 @@ import { StaticItemDisplay } from '../utils/staticItem';
                 </div>
             </div>
         </article>
+        <section class="w-full my-2 flex items-center justify-center">
+            <div class="flex">
+                <div class="w-12 h-12 bg-slate-100 text-indigo-700 drop-shadow-md hover:bg-indigo-700 hover:text-slate-100 hover:drop-shadow-none rounded-md flex items-center justify-center">
+                    <span>1</span>
+                </div>
+                <div class="w-12 h-12 bg-indigo-700 text-slate-100 hover:bg-slate-100 hover:text-indigo-700 hover:drop-shadow-md rounded-md flex items-center justify-center">
+                    <span>2</span>
+                </div>
+                <div class="w-12 h-12 bg-indigo-700 rounded-md hover:bg-slate-100 hover:text-indigo-700 hover:drop-shadow-md flex items-center justify-center">
+                    <span class="text-slate-100">3</span>
+                </div>
+            </div>
+        </section>
     </section>
 </template>
--- src/views/centralShopView.vue
+++ src/views/centralShopView.vue
@@ -4,4 +4,17 @@ import ShopItemListVue from '../components/ShopItemList.vue';
 
 <template>
     <ShopItemListVue></ShopItemListVue>
+    <section class="w-full my-2 flex items-center justify-center">
+        <div class="flex">
+            <div class="w-12 h-12 bg-slate-100 text-indigo-700 drop-shadow-md hover:bg-indigo-700 hover:text-slate-100 hover:drop-shadow-none rounded-md flex items-center justify-center">
+                <span>1</span>
+            </div>
+            <div class="w-12 h-12 bg-indigo-700 text-slate-100 hover:bg-slate-100 hover:text-indigo-700 hover:drop-shadow-md rounded-md flex items-center justify-center">
+                <span>2</span>
+            </div>
+            <div class="w-12 h-12 bg-indigo-700 rounded-md hover:bg-slate-100 hover:text-indigo-700 hover:drop-shadow-md flex items-center justify-center">
+                <span class="text-slate-100">3</span>
+            </div>
+        </div>
+    </section>
 </template>
