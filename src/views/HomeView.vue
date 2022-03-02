ce7f677293e90b7825bb8b092c5665e9ceed3b0f
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-02 16:49:37 +0100

    :building_construction: backend init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import { HeartIcon } from '@heroicons/vue/solid';
 </script>
 
 <template>
-    <section class=" px-28  flex flex-row justify-between h-auto py-28"> <!-- SHOP WRAPPER -->
+<!--     <section class=" px-28  flex flex-row justify-between h-auto py-28">
         <div class="w-3/6 flex flex-col justify-between py-16 pr-8">
             <h1 class="text-red-500 text-6xl text-right">&lt;3 GOODIES</h1>
             <h3 class="text-right  ml-32">
@@ -17,6 +18,12 @@ import { RouterLink } from 'vue-router';
         </div>
         <div class="">
             <img src="../assets/img_01.png" alt="">
+        </div>
+    </section> -->
+    <section class="w-full h-screen">
+        <div class="flex items-center"> <!-- TITLE WRAPPER-->
+            <HeartIcon class="w-24 h-24 text-red-500 mx-4"></HeartIcon>
+            <h1 class="text-red-500 text-6xl uppercase font-extrabold">GOODIES</h1>
         </div>
     </section>
 </template>
